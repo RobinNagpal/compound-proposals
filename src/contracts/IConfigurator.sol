f4fc0ba42893186aec56a5095c8ce5d793690296
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-04-07 15:36:44 +0500

    IConfigurator interface

---
+++ src/contracts/IConfigurator.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+import './structs.sol';
+
+interface IConfigurator {
+    function addAsset(address cometProxy, Structs.AssetConfig calldata assetConfig) external;
+
+    function setGovernor(address cometProxy, address newGovernor) external;
+
+    function getAssetIndex(address cometProxy, address asset) external returns (uint256);
+}
