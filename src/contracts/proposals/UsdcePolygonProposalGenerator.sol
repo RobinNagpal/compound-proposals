6761f4c11778a32780b370e786a11ec80d21659d
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-04-29 19:38:52 +0500

    fixed compilation error and marked function as being overridden

--- src/contracts/proposals/UsdcePolygonProposalGenerator.sol
+++ src/contracts/proposals/UsdcePolygonProposalGenerator.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import './../structs.sol';
 import {ProposalGenerator} from './ProposalGenerator.sol';
 import './MarketConfig.sol';
 import {GovernanceV3Polygon} from '../compoundAddresses/GovernanceV3Polygon.sol';
@@ -14,7 +15,7 @@ abstract contract UsdcePolygonProposalGenerator is
     })
   )
 {
-  function createProposalPayload() public view returns (Structs.ProposalInfo memory) {
+  function createProposalPayload() public view override returns (Structs.ProposalInfo memory) {
     Structs.ProposalInfo memory polygonProposalInfo = super.createProposalPayload();
 
     bytes memory message = abi.encode(
@@ -31,7 +32,7 @@ abstract contract UsdcePolygonProposalGenerator is
     proposalInfo.signatures = new string[](1);
     proposalInfo.calldatas = new bytes[](1);
 
-    proposalInfo.targets[0] = address(MARKET_CONFIG.BRIDGE_RECEIVER);
+    proposalInfo.targets[0] = address(GovernanceV3Polygon.BRIDGE_RECEIVER);
     proposalInfo.values[0] = 0;
     proposalInfo.signatures[0] = 'sendMessageToChild(address,bytes)';
     proposalInfo.calldatas[0] = abi.encode(GovernanceV3Polygon.CONFIGURATOR_PROXY, message);
