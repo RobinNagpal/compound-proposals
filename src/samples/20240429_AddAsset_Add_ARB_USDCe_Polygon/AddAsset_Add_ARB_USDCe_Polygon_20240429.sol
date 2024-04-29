4df14cc1ce2cd6a657c44cb1221a7015af65f082
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-04-29 21:14:08 +0500

    polygon files

---
+++ src/samples/20240429_AddAsset_Add_ARB_USDCe_Polygon/AddAsset_Add_ARB_USDCe_Polygon_20240429.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import {IConfigurator} from 'src/contracts/IConfigurator.sol';
+import {ICometProxyAdmin} from 'src/contracts/ICometProxyAdmin.sol';
+import {Structs} from 'src/contracts/structs.sol';
+import {CommonTestBase} from 'src/contracts/CommonTestBase.sol';
+import {VmSafe} from 'forge-std/Vm.sol';
+import 'forge-std/console.sol';
+import {UsdcePolygonProposalGenerator} from '../../contracts/proposals/UsdcePolygonProposalGenerator.sol';
+import '../../contracts/proposals/MarketConfig.sol';
+
+/**
+ * @title Add_ARB
+ * @author Robin Nagpal
+ * - Discussion: TODO
+ */
+contract AddAsset_Add_ARB_USDCe_Polygon_20240429 is UsdcePolygonProposalGenerator {
+  function getNewAssetsConfigs() public pure override returns (Structs.AssetConfig[] memory) {
+    Structs.AssetConfig[] memory configs = new Structs.AssetConfig[](1);
+
+    configs[0] = Structs.AssetConfig({
+      asset: address(0xae78736Cd615f374D3085123A210448E74Fc6393),
+      priceFeed: address(0xA3A7fB5963D1d69B95EEC4957f77678EF073Ba08),
+      decimals: 18,
+      borrowCollateralFactor: 900,
+      liquidateCollateralFactor: 930000000000000000,
+      liquidationFactor: 970000000000000000,
+      supplyCap: 300000000000000000
+    });
+    return configs;
+  }
+}
