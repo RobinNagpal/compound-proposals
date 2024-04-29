d4830ddbcbc3a14c81f6eeb945068eb8336ca1fd
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-04-29 21:13:52 +0500

    renamed files and removed all addresses

---
+++ src/samples/20240412_AddAsset_Add_rETH_Weth_Mainnet/AddAsset_Add_rETH_Weth_Mainnet_20240412.t.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import {AddAsset_Add_rETH_Weth_Mainnet_20240412} from './AddAsset_Add_ARB_Weth_Mainnet_20240412.sol';
+import {CommonTestBase} from 'src/contracts/CommonTestBase.sol';
+import {IConfigurator} from 'src/contracts/IConfigurator.sol';
+import {VmSafe} from 'forge-std/Vm.sol';
+import 'forge-std/console.sol';
+import 'forge-std/Test.sol';
+import {IProposalGenerator} from 'src/contracts/proposals/IProposalGenerator.sol';
+import 'src/contracts/structs.sol';
+import {GovernanceV3MainnetAssets, GovernanceV3Mainnet} from '../../contracts/compoundAddresses/GovernanceV3Mainnet.sol';
+
+/**
+ * @dev Test for AddAsset_Add_rETH_Weth_Mainnet_20240412
+ * command: make test-contract filter=AddAsset_Add_rETH_Weth_Mainnet_20240412
+ */
+contract AddAsset_Add_rETH_Weth_Mainnet_20240412 is CommonTestBase {
+  AddAsset_Add_rETH_Weth_Mainnet_20240412 internal proposal;
+  IConfigurator configurator;
+
+  function setUp() public {
+    vm.createSelectFork(vm.rpcUrl('mainnet'), 18544451);
+    proposal = new AddAsset_Add_rETH_Weth_Mainnet_20240412();
+    configurator = IConfigurator(GovernanceV3Mainnet.CONFIGURATOR_ADDRESS);
+  }
+
+  function isAssetListed() internal returns (bool) {
+    try
+      configurator.getAssetIndex(
+        GovernanceV3Mainnet.WETH_COMET_PROXY,
+        GovernanceV3MainnetAssets.RETH_TOKEN
+      )
+    returns (uint256 assetIndex) {
+      return true;
+    } catch (bytes memory lowLevelData) {
+      emit log('Asset is not listed');
+    }
+    return false;
+  }
+
+  function testAddAsset() public {
+    require(!isAssetListed(), 'Asset should not be listed before execution.');
+    vm.startPrank(GovernanceV3Mainnet.TIMELOCK);
+    Structs.ProposalInfo memory proposalInfo = proposal.createProposalPayload();
+    executeProposal(proposalInfo);
+    vm.stopPrank();
+  }
+}
