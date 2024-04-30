5ff1c4b548d977808307613e5f6663ff3c89c229
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-04-30 21:20:48 +0500

    updated imports resolver

--- src/generator/utils/importsResolver.ts
+++ src/generator/utils/importsResolver.ts
@@ -1,15 +1,27 @@
+import {Market} from '../types';
+
 /**
  * @dev Returns the input string prefixed with imports
  * @param code
  * @returns
  */
 let imports = '';
-export function prefixWithImports(code: string) {
+export function prefixWithImports(market: Market, code: string, type: string = 'proposal' || 'test') {
   imports += `import {IConfigurator} from 'src/contracts/IConfigurator.sol';\n`;
-  imports += `import {ICometProxyAdmin} from 'src/contracts/ICometProxyAdmin.sol';\n`;
   imports += `import {Structs} from 'src/contracts/structs.sol';\n`;
-  imports += `import {CommonTestBase} from 'src/contracts/CommonTestBase.sol';\n`;
-  imports += `import {VmSafe} from 'forge-std/Vm.sol';\n`;
-  imports += `import 'forge-std/console.sol';\n`;
+  imports += `import {GovernanceV3${market.chain}, GovernanceV3${market.chain}Assets} from 'src/contracts/compoundAddresses/GovernanceV3Mainnet.sol';\n`;
+
+  if (type === 'proposal') {
+    imports += `import {ICometProxyAdmin} from 'src/contracts/ICometProxyAdmin.sol';\n`;
+    imports += `import {${market.baseAsset}${market.chain}ProposalGenerator} from 'src/contracts/proposals/${market.baseAsset}${market.chain}ProposalGenerator.sol';\n`;
+  }
+
+  if (type === 'test') {
+    imports += `import {CommonTestBase} from 'src/contracts/CommonTestBase.sol';\n`;
+    imports += `import 'src/contracts/proposals/MarketConfig.sol';\n`;
+    imports += `import {VmSafe} from 'forge-std/Vm.sol';\n`;
+    imports += `import 'forge-std/Test.sol';\n`;
+  }
+
   return imports + code;
 }
