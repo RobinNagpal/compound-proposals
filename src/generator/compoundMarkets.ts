87353d872ab6a13d33fd0f213e69113d739392ab
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-05-02 22:56:41 +0500

    added new tokens in compound markets

--- src/generator/compoundMarkets.ts
+++ src/generator/compoundMarkets.ts
@@ -1,5 +1,5 @@
-export const MAINNET_KNOWN_ASSETS = ['WETH', 'USDC', 'COMP', 'LINK', 'UNI', 'WBTC', 'RETH'];
-export const POLYGON_KNOWN_ASSETS = ['WETH', 'WBTC', 'WMATIC', 'LINK'];
+export const MAINNET_KNOWN_ASSETS = ['WETH', 'USDC', 'COMP', 'LINK', 'UNI', 'WBTC', 'RETH', 'BNB', 'ARB', 'STETH'];
+export const POLYGON_KNOWN_ASSETS = ['WETH', 'WBTC', 'WMATIC', 'LINK', 'BNB', 'SHIB', 'AVAX', 'UNI', 'AAVE', 'MATIC'];
 
 export type MainnetAsset = (typeof MAINNET_KNOWN_ASSETS)[number];
 export type PolygonAsset = (typeof POLYGON_KNOWN_ASSETS)[number];
