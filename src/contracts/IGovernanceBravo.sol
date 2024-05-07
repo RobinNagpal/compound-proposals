b0fc173a85a21bb67d534c9c40e7bf944f71557e
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-05-07 19:14:38 +0500

    added new functions in governor bravo interface

--- src/contracts/IGovernanceBravo.sol
+++ src/contracts/IGovernanceBravo.sol
@@ -3,10 +3,12 @@ pragma solidity ^0.8.15;
 
 interface IGovernanceBravo {
   function propose(
-    address[] memory targets,
-    uint[] memory values,
-    string[] memory signatures,
-    bytes[] memory calldatas,
-    string memory description
+    address[] calldata targets,
+    uint[] calldata values,
+    string[] calldata signatures,
+    bytes[] calldata calldatas,
+    string calldata description
   ) external returns (uint);
+  function _setWhitelistAccountExpiration(address account, uint expiration) external;
+  function isWhitelisted(address account) external returns (bool);
 }
